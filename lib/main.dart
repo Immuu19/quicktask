6da9ce41b3c6ebe36fec0e260274a151dbacfb83
Immuu19 <mohammedmrn7@gmail.com>
2024-11-28 02:29:33 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,8 @@ import 'screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  const applicationId = 'cd0uRAditBGbdJmfO0BwPK0w7u5FNMTBv92ykN8n';
-  const clientKey = 'nDxwb3nprkIWd2c331iaxi4yKtkKHR3QFgvHM5hu';
+  const applicationId = '';
+  const clientKey = '';
   const serverUrl = 'https://parseapi.back4app.com/';
 
   await Parse().initialize(applicationId, serverUrl, clientKey: clientKey);
